86656821e790b3aad31b1920cc7b5b63a6eb3461
imoempire <mrimoempire@gmail.com>
2021-06-22 13:35:42 +0000

    add code changes

--- src/Components/ClassCounter.js
+++ src/Components/ClassCounter.js
@@ -6,20 +6,24 @@ class ClassCounter extends Component {
         this.state = {
             count: 0
         };
-    }
+    }  
     render() {
-        const handleIncrease = ()=>{
+        handleIncrease=()=>{
             this.setState(this.state.count +1)
         };
-      const handleDecrease =() => {
+       handleDecrease=() => {
           this.setState(this.state.count - 1)};
-      
+
         return (
             <div>
                 <div style={{ textAlign: "center" }}>
                     <h1>Count: {this.state.count}</h1>
+                    <button onClick={() => this.setState({ count: this.state.count + 1 })}>Increase</button>
+                    <button onClick={() => this.setState({ count: this.state.count + 1 })}>Decrease</button>
+                    <br />
                     <button onClick={handleIncrease}>Increase</button>
                     <button onClick={handleDecrease}>Decrease</button>
+
                 </div>
 
             </div>
